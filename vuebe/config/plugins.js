ca93d53a1290ee690c68ba274292b8b91d9c28d4
zaidanki <44997660+zaidanki@users.noreply.github.com>
2021-08-26 20:40:14 +0300

    cloudinary

--- vuebe/config/plugins.js
+++ vuebe/config/plugins.js
@@ -3,14 +3,11 @@ module.exports = ({ env }) => {{
     if(env('NODE_ENV') === 'production'){
         return {
             upload: {
-                provider: 'aws-s3',
+                provider: 'cloudinary',
                 providerOptions: {
-                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
-                    params: {
-                        Bucket: env('AWS_BUCKET')
-                    }
+                    cloud_name: env('CLOUDINARY_NAME'),
+                    api_key: env('CLOUDINARY_API_KEY'),
+                    api_secret: env('CLOUDINARY_API_SECRET'),
                 }
             }
         }
